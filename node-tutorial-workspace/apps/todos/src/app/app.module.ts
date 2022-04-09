fe20dc6ccf0147ec63df4a5dab2d879bb2c8c6a7
Oğuzcan Budumlu <obudumlu@gmail.com>
2022-04-09 17:39:38 +0300

    sample auth module added

--- node-tutorial-workspace/apps/todos/src/app/app.module.ts
+++ node-tutorial-workspace/apps/todos/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoService } from './todo/todo.service';
+import { AuthModule } from '@node-tutorial-workspace/auth';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService, TodoService],
 })
