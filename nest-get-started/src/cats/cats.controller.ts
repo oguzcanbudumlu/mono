72731ff48d28c56af003fe5517200e1c05aebd42
Oğuzcan Budumlu <obudumlu@gmail.com>
2022-04-09 20:15:08 +0300

    some cats endpoints added

--- nest-get-started/src/cats/cats.controller.ts
+++ nest-get-started/src/cats/cats.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Query,
+  Redirect,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
@@ -8,8 +18,44 @@ export class CatsController {
     return 'This action adds a new cat';
   }
 
+  @Post()
+  @HttpCode(204)
+  createWithDifferentResponse() {
+    return 'This action adds a new cat';
+  }
+
+  @Post()
+  @Header('Cache-Control', 'none')
+  createWithHeader() {
+    return 'This action adds a new cat';
+  }
+
   @Get()
   findAll(@Req() request: Request): string {
     return 'This action returns all cats';
+  }
+
+  @Get('ab*cd')
+  findAllWithPattern() {
+    return 'This route uses a wildcard';
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
+
+  @Get(':id')
+  findOneWithAnotherWay(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
   }
 }
