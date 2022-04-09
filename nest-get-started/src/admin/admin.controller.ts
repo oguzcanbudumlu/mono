7b03147eddbf9fb6715e59916cb111f07536ec67
Oğuzcan Budumlu <obudumlu@gmail.com>
2022-04-09 20:39:36 +0300

    admin controller updated

--- nest-get-started/src/admin/admin.controller.ts
+++ nest-get-started/src/admin/admin.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('admin')
-export class AdminController {}
+@Controller({ host: 'admin.example.com' })
+export class AdminController {
+  @Get()
+  index(): string {
+    return 'Admin page';
+  }
+}
