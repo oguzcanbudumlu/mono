23e442308e24977ac10b6a00a23c910af245e086
Oğuzcan Budumlu <obudumlu@gmail.com>
2022-04-11 18:08:14 +0300

    some simple modules added

--- nest-get-started/src/app.module.ts
+++ nest-get-started/src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { AdminController } from './admin/admin.controller';
 import { CatsService } from './cats/cats.service';
+import { DogsModule } from './dogs/dogs.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [DogsModule, CommonModule],
   controllers: [AppController, CatsController, AdminController],
   providers: [AppService, CatsService],
+  exports: [CommonModule],
 })
 export class AppModule {}
