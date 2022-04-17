980b6643c9ab68a30f503764746c87337f1f25a5
Oğuzcan Budumlu <obudumlu@gmail.com>
2022-04-17 20:39:03 +0300

    query service implemented

--- posts-comments-microservices/event-bus/index.js
+++ posts-comments-microservices/event-bus/index.js
@@ -5,20 +5,15 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const event = req.body;
 
   console.log("event", event);
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+  await axios.post("http://localhost:4000/events", event);
+  await axios.post("http://localhost:4001/events", event);
+  await axios.post("http://localhost:4002/events", event);
 
   res.send({ status: "OK" });
-});
-
-app.post("/events", (req, res) => {
-  console.log("Received Event", req.body.type);
-  res.send({});
 });
 
 app.listen(4005, () => {
