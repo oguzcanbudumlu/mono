bd3218f70d10627c5439e5b2e45f3ef83bc694d6
Oğuzcan Budumlu <obudumlu@gmail.com>
2022-04-18 20:26:46 +0300

    missing events handled

--- posts-comments-microservices/event-bus/index.js
+++ posts-comments-microservices/event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", async (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   console.log("event", event);
   await axios.post("http://localhost:4000/events", event);
@@ -15,6 +19,10 @@ app.post("/events", async (req, res) => {
   await axios.post("http://localhost:4003/events", event);
 
   res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- posts-comments-microservices/query/index.js
+++ posts-comments-microservices/query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  console.log("OK");
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -32,10 +27,26 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  console.log("OK");
+  const { type, data } = req.body;
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+
+  const res = await axios.get("http://localhost:4005/events");
+  res.data.forEach((event) => {
+    console.log("Processing event", event.type);
+    handleEvent(event.type, event.data);
+  });
 });
